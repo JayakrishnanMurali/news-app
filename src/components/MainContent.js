64104953fd853441c56a6cc8222d49bdde147103
Jayakrishnan M <jayakrishnan1012@gmail.com>
2021-11-02 23:33:54 +0530

    Added main Component UI

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { News } from "./News";
 
 const MainContent = () => {
   return (
-    <div className="bg-red-200 max-h-96 overflow-y-scroll">
-      <h1>Check</h1>
+    <div className="p-4 border-2 h-[30rem] overflow-y-scroll">
+      <News />
+      <News />
     </div>
   );
 };
